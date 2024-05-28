230066726f808277ac6274d181bc8cee6c5ec44f
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2024-05-28 18:38:15 -0400

    reorganize project for vercel deployment

--- api/server.js
+++ api/server.js
@@ -4,12 +4,14 @@ const schema = require('./schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+app.use('/api/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
 
 const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}/graphql`);
+  console.log(`Server is running on http://localhost:${PORT}/api/graphql`);
 });
+
+module.exports = app;
